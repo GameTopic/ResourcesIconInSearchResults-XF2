03bb1edcb86ead8f55c171ed236c561a11e06223
NICK97 <40327737+bolandnick18@users.noreply.github.com>
2020-07-05 13:14:17 -0700

    adding new data

---
+++ upload/src/addons/nick97/ResourceIconInSearchResults/Setup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace nick97\ResourceIconInSearchResults;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
